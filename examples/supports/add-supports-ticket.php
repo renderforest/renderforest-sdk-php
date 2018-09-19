bec3a02000f4b5e48c43b33f64bb214e7533705e
narekhovhannisyan <narekgilmour@gmail.com>
2018-09-19 14:32:29 +0400

    examples(supports): add example for adding support ticket
     - examples(templates): rename get-templates-categories
     - fixes #40

---
+++ examples/supports/add-supports-ticket.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) 2018-present, Renderforest, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory.
+ */
+
+require_once(dirname(__FILE__) . '/../../src/lib/Renderforest.php');
+
+$options = ['signKey' => '<signKey>', 'clientId' => -1];
+
+$renderforest = new Renderforest($options);
+
+$payload = [
+    'message' => 'I need to...',
+    'mailType' => 'Creative team',
+    'subject' => 'Some help in ..'
+];
+
+try {
+    $currentUser = $renderforest->addSupportsTicket($payload);
+} catch (Exception $e) {
+    var_dump($e); // handle the error
+}
+
+var_dump($currentUser); // handle the success
