1a3a8828744313b669d5978bce145c39c97616e5
narekhovhannisyan <narekgilmour@gmail.com>
2018-09-20 17:35:57 +0400

    examples(sounds): fix variable name in get-company-sounds-limited

--- examples/sounds/get-company-sounds-limited.php
+++ examples/sounds/get-company-sounds-limited.php
@@ -14,9 +14,9 @@ $payload = [
 ];
 
 try {
-    $currentUser = Renderforest::getCompanySoundsLimited($payload);
+    $companySoundsLimited = Renderforest::getCompanySoundsLimited($payload);
 } catch (Exception $e) {
     var_dump($e); // handle the error
 }
 
-var_dump($currentUser); // handle the success
+var_dump($companySoundsLimited); // handle the success
