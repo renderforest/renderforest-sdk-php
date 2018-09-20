7f437caa7a6cd1ac08feccf72f6afe8dd955737c
narekhovhannisyan <narekgilmour@gmail.com>
2018-09-20 17:14:23 +0400

    examples(templates): fix variable name in get-templates-categories

--- examples/templates/get-templates-categories.php
+++ examples/templates/get-templates-categories.php
@@ -14,9 +14,9 @@ $payload = [
 ];
 
 try {
-    $currentUser = Renderforest::getTemplatesCategories($payload);
+    $templateCategories = Renderforest::getTemplatesCategories($payload);
 } catch (Exception $e) {
     var_dump($e); // handle the error
 }
 
-var_dump($currentUser); // handle the success
+var_dump($templateCategories); // handle the success
