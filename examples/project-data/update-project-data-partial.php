c77f1ea55590850b487247e8114747e84b2a2205
narekhovhannisyan <narekgilmour@gmail.com>
2018-10-08 18:21:59 +0400

    examples(update-project-data): add example for updating project data  partially
     - fixes #26

---
+++ examples/project-data/update-project-data-partial.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright (c) 2018-present, Renderforest, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory.
+ */
+
+require_once(dirname(__FILE__) . '/../../src/lib/Renderforest.php');
+
+$options = ['signKey' => '<signKey>', 'clientId' => -1];
+
+$renderforest = new Renderforest($options);
+
+$payload = [
+    'projectId' => 7549843
+];
+
+function sample()
+{
+    global $renderforest;
+    global $payload;
+    $projectDataInstance = $renderforest->getProjectData($payload);
+
+    // make some change
+    $projectDataInstance->setMuteMusic(true);
+
+    // get payload data
+    $projectId = $projectDataInstance->getProjectId();
+    $data = $projectDataInstance->getPatchObject();
+
+    $payloadForUpdate = [
+        'projectId' => $projectId,
+        'data' => $data
+    ];
+
+    $result = $renderforest->updateProjectDataPartial($payloadForUpdate);
+
+    $projectDataInstance->resetPatchObject();
+
+    return $result;
+}
+
+try {
+    var_dump(sample()); // handle the success
+} catch (Exception $e) {
+    var_dump($e); // handle the error
+}
