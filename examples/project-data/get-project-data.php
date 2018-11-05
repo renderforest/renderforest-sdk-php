0c96a9eb77453c1b68974becd5d27b784c8f4d66
narekhovhannisyan <narekgilmour@gmail.com>
2018-11-05 18:24:56 +0400

    examples(project-data): update examples

--- examples/project-data/get-project-data.php
+++ examples/project-data/get-project-data.php
@@ -9,14 +9,11 @@
 
 require 'vendor/autoload.php';
 
-$options = ['signKey' => '<signKey>', 'clientId' => -1];
-
-$renderforest = new \Renderforest\Client($options);;
+$renderforest = new \Renderforest\Client(['signKey' => '<signKey>', 'clientId' => -1]);
 
 $payload = [
     'projectId' => 7549843
 ];
-
 try {
     $projectDataInstance = $renderforest->getProjectData($payload);
 } catch (\GuzzleHttp\Exception\GuzzleException $e) {
