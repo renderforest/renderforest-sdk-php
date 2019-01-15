d74ead5ffd569f7db8053291e5becfc9d56dab6d
narekhovhannisyan <narekgilmour@gmail.com>
2019-01-15 17:11:55 +0400

    examples(templates): add example for getTemplateSVGContent method.
     - fixes #113

---
+++ examples/templates/get-template-svg-content.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2018-present, Renderforest, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory.
+ */
+
+require 'vendor/autoload.php';
+
+$payload = [
+    'templateId' => 701
+];
+try {
+    $templateSVGContent = \Renderforest\Client::getTemplateSVGContent($payload);
+} catch (\GuzzleHttp\Exception\GuzzleException $e) {
+    var_dump($e); // handle the error
+}
+
+var_dump($templateRecommendedCustomColors); // handle the success
