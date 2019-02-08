23a9cb97338acf3f83c9573c4dd10624977227d2
narekhovhannisyan <narekgilmour@gmail.com>
2019-02-08 13:50:36 +0400

    examples(projects): add delete project videos example.

---
+++ examples/projects/delete-project-videos.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright (c) 2018-present, Renderforest, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory.
+ */
+
+require 'vendor/autoload.php';
+
+$renderforest = new \Renderforest\Client(['signKey' => '<signKey>', 'clientId' => -1]);
+
+$payload = [
+    'projectId' => 5000658,
+    'quality' => 360 // optional argument
+];
+try {
+    $deleteProject = $renderforest->deleteProjectVideos($payload);
+} catch (\GuzzleHttp\Exception\GuzzleException $e) {
+    var_dump($e); // handle the error
+}
+
+var_dump($deleteProject); // handle the success
