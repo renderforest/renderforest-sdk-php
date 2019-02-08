a4d3e04e6473d8c173b3ed10993dc9951f248254
narekhovhannisyan <narekgilmour@gmail.com>
2019-02-08 13:50:01 +0400

    src(Params): add destructOptionalURLParam method, fix PhpDocs.

--- src/Params.php
+++ src/Params.php
@@ -6,6 +6,7 @@
  * This source code is licensed under the license found in the
  * LICENSE file in the root directory.
  */
+
 namespace Renderforest;
 
 use Renderforest\Error\RenderforestError;
@@ -13,10 +14,10 @@ use Renderforest\Error\RenderforestError;
 class Params
 {
     /**
-     * @param array $payload
-     * @param array $props
+     * Destructs given properties from the payload.
+     * @param array $payload - The array to destruct.
+     * @param array $props - The prop to destruct from array.
      * @return array
-     * @description Destruct given properties from the payload.
      */
     public function destructParams($payload, $props)
     {
@@ -34,16 +35,36 @@ class Params
     }
 
     /**
-     * @param array $payload
-     * @param string $param
-     * @return mixed
+     * Destructs URL param from the payload.
+     * @param array $payload - The array to destruct.
+     * @param string $param - The param to destruct from array.
+     * @return string
      * @throws RenderforestError
-     * @description Destruct URL param from the payload.
      */
     public function destructURLParam($payload, $param)
     {
         if (!isset($payload) || !sizeof($payload) || !isset($payload[$param])) {
             throw new RenderforestError("Missing required parameter: ${param}.");
+        }
+
+        return $payload[$param];
+    }
+
+    /**
+     * Destructs optional URL param from the given payload.
+     * @param $payload - Thr array to destruct.
+     * @param $param - The param to destruct from array.
+     * @return string
+     * @throws RenderforestError
+     */
+    public function destructOptionalURLParam($payload, $param)
+    {
+        if (!isset($payload) || sizeof($payload)) {
+            throw new RenderforestError("Missing optional parameter: ${param}.");
+        }
+
+        if (!isset($payload[$param])) {
+            return '';
         }
 
         return $payload[$param];
