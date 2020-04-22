5844b57766f0abee76abdf1af02518c5d5e0fb08
Artyom <artyom@ymail.com>
2020-04-22 16:15:29 +0400

    examples(sounds): add getRecommendedSoundsLimited example.

---
+++ examples/sounds/get-recommended-sounds-limited.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../../vendor/autoload.php';
+
+$templateId = 701;
+//Preferred sound duration
+$duration = 60;
+
+$sounds = \Renderforest\ApiClient::getRecommendedSoundsLimited($templateId, $duration);
+
+echo 'Count - ' . count($sounds) . PHP_EOL;
+echo PHP_EOL;
+
+foreach ($sounds as $sound) {
+    echo 'ID - ' . $sound->getId() . PHP_EOL;
+    echo 'Duration - ' . $sound->getDuration() . PHP_EOL;
+    echo 'Title - ' . $sound->getTitle() . PHP_EOL;
+    echo 'Path - ' . $sound->getPath() . PHP_EOL;
+    echo 'Low Quality Path - ' . $sound->getLowQuality() . PHP_EOL;
+    echo 'Genre - ' . $sound->getGenre() . PHP_EOL;
+
+    echo PHP_EOL;
+}
