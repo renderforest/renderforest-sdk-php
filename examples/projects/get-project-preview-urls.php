0b164916d7efc61ab51c634d8f758cd45af7b624
Hrach Mkrtchyan <hrachmkr96@gmail.com>
2024-09-25 10:32:36 +0400

    Update php version

--- examples/projects/get-project-preview-urls.php
+++ examples/projects/get-project-preview-urls.php
@@ -3,8 +3,8 @@
 require '../../vendor/autoload.php';
 
 $renderforestClient = new \Renderforest\ApiClient(
-    "fbcc4dc6a09e47ffdb3852311116b6fb",
-    "735823"
+    'your-api-key',
+    'your-client-id'
 );
 
 $projectId = 88172301; // Replace with your actual project ID
